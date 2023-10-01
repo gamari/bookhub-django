5b6bab726e0717f3568d018406436f6a8c274591
gamari <gamari4552@gmail.com>
2023-10-01 23:50:10 +0000

    rating

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('account/<slug:username>/', views.account_detail, name='account_detail'),
+    path('account/<str:username>/', views.account_detail, name='account_detail'),
 
     path('login/', views.login_view, name='login'),
     path("register/", views.register_view, name="register"),
