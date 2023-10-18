01c38ce8cef0db4c1d391b784f47a533b9da9087
gamari <gamari4552@gmail.com>
2023-10-18 16:36:32 +0000

    レビュー一覧の作成

---
+++ apps/review/templatetags/review_tags.py
@@ -0,0 +1,12 @@
+from django import template
+from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
+
+register = template.Library()
+
+@register.simple_tag
+def review_list(reviews):
+    context = { 'reviews': reviews }
+    return mark_safe(
+        render_to_string('components/_review_list.html', context)
+    )
