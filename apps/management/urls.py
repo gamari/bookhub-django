1440341cf2257080b81a63946d19faa1d3fdd1db
gamari <gamari4552@gmail.com>
2023-10-24 20:12:38 +0000

    編集ページの作成

--- apps/management/urls.py
+++ apps/management/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('manage/dashboard/', views.management_dashboard, name='management_dashboard'),
+    path('manage/books/', views.management_books, name='management_books'),
+    path('manage/book/<int:book_id>/', views.management_book_edit, name='management_book_edit'),
 ]
--- apps/management/views.py
+++ apps/management/views.py
@@ -2,7 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import user_passes_test
 
+from apps.book.models import Book
+
 @user_passes_test(lambda u: u.is_superuser)
 def management_dashboard(request):
     context = {}
     return render(request, "pages/manage-dashboard.html", context)
+
+@user_passes_test(lambda u: u.is_superuser)
+def management_books(request):
+    books = Book.objects.all().filter(is_clean=False).order_by("-views")[:10]
+    context = {
+        "books": books,
+    }
+    return render(request, "pages/manage-books.html", context)
+
+# 書籍編集ページ
+@user_passes_test(lambda u: u.is_superuser)
+def management_book_edit(request, book_id):
+    book = Book.objects.get(id=book_id)
+    context = {
+        "book": book,
+    }
+    return render(request, "pages/manage-book-edit.html", context)
