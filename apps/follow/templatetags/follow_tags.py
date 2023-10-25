fa7816299adabf47ffcda0c312524a61c88421b8
gamari <gamari4552@gmail.com>
2023-10-25 07:59:19 +0000

    フォローボタンが表示される問題の修正

--- apps/follow/templatetags/follow_tags.py
+++ apps/follow/templatetags/follow_tags.py
@@ -1,7 +1,11 @@
+import logging
+
 from django import template
 from django.template.loader import render_to_string
 
 from apps.follow.domain.services import FollowService
+
+logger = logging.getLogger("app_logger")
 
 register = template.Library()
 
@@ -13,9 +17,16 @@ def is_following(user, target_user):
 def follow_button(user, target):
     follow_service = FollowService()
     is_following = follow_service.is_following(user.id, target.id)
+    is_self = user.id == target.id
+    
+    logger.debug(user.id)
+    logger.debug(target.id)
+    logger.debug(is_self)
 
     return render_to_string('tags/follow_button.html', {
         'user': user,
         'target_account': target,
-        'is_following': is_following
+        'is_following': is_following,
+        'is_self': is_self,
+        "is_authenticated": user.is_authenticated,
     })
