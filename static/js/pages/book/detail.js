3c64f09618e0bc506997f11380ff938b1778753b
gamari <gamari4552@gmail.com>
2023-10-30 00:13:16 +0000

    書籍紹介のタブを追加

--- static/js/pages/book/detail.js
+++ static/js/pages/book/detail.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const btnReviews = document.getElementById('btn-reviews');
     const btnTimeline = document.getElementById('btn-timeline');
+    const btnBookDetail = document.getElementById('btn-book-detail');
     
     const sectionReviews = document.getElementById('reviews-section');
     const sectionTimeline = document.getElementById('timeline-section');
+    const sectionBookDetail = document.getElementById('book-detail-section');
 
     let timelinePage = 1;
 
@@ -19,10 +21,18 @@ document.addEventListener('DOMContentLoaded', function() {
         btnTimeline.classList.add('badge-info');
     });
 
+    btnBookDetail.addEventListener('click', function () {
+        reset();
+        sectionBookDetail.style.display = 'block';
+        btnBookDetail.classList.add('badge-info');
+    });
+
     function reset() {
         sectionReviews.style.display = 'none';
         sectionTimeline.style.display = 'none';
+        sectionBookDetail.style.display = 'none';
         btnReviews.classList.remove('badge-info');
         btnTimeline.classList.remove('badge-info');
+        btnBookDetail.classList.remove('badge-info');
     }
 });
